7dd8843d368cba8976a0bad0e1b70d44c134c6f1
Alice Lemerre <al.lemerre@laposte.net>
2024-07-07 22:17:45 +0200

    fix StripeTransactionFactory

--- App/Provider/Stripe/StripeTransactionFactory.php
+++ App/Provider/Stripe/StripeTransactionFactory.php
@@ -15,29 +15,14 @@ class StripeTransactionFactory extends StripeTransaction
         float $amount,
         int $status,
     )
+    {
+        return new StripeTransaction(
+            $id,
+            $description,
+            $currency,
+            $amount,
+            $status,
+        );}
 }
 
-?> 
-
-<?php
-
-namespace App\Payment;
-
-use App\Payment\Stripe\StripePayment;
-
-abstract class PaymentFactory
-{
-    public function __construct(
-        private string $gateway
-    )
-    {
-    }
-
-    public function gateway(): StripePayment
-    {
-        return match ($this->gateway) {
-            'stripe' => new StripePayment(),
-            default => throw new \InvalidArgumentException('Unknown gateway'),
-        };
-    }
-}
+?>
