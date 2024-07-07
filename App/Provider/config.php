27cc7567c958c85c2989affeb59070b81eb28000
Alice Lemerre <al.lemerre@laposte.net>
2024-07-07 22:13:45 +0200

    implémentation de la librairie

--- App/Provider/config.php
+++ App/Provider/config.php
@@ -1,3 +1,3 @@
 <?php
 
-define('STRIPE_SECRET_KEY', 'sk_test_51PU9jRRu8yJrd6Ll7vTEfeTagbI0zJNTK6EsEreG2cukyyZaMmlsZx1b9Lkj10ozlbKQxCqul39TJqhzWMNLzLQy00FdJ9OqpX'); //mettez ici votre clé d'API secrète Stripe (https://dashboard.stripe.com/test/apikeys)
+define('STRIPE_SECRET_KEY', ''); //mettez ici votre clé d'API secrète Stripe (https://dashboard.stripe.com/test/apikeys)
